dda6cd6aa2b043f3a9644f609366989a4c73346f
paco <74721116+Zagred@users.noreply.github.com>
2023-04-20 22:36:30 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -20,12 +20,53 @@ struct wav_header{
    int ByteRate;
    short BlockAlign;
    short BitsPerSample;
-   int Subchunk2ID;
+   char Subchunk2ID;
    int Subchunk2Size;
 };
 #pragma pack(pop)
-static int s_muted=0;
-static char unit='B';
+static int s_muted=0,counter=0;
+static char unit='B',choice[1000];
+//4-ta tochka
+short * stereoChannel;
+short * leftChannel;
+short * rightChannel;
+int     val     =    50000;
+
+void part4(FILE *file,struct wav_header*header){ 
+  while (1)
+  {
+    printf("left or right\n");
+    scanf("%s",&choice);
+    if(choice[0]=='l'||choice[0]=='r'){
+      break;
+    }
+  }
+  printf("choice:%s\n",choice);
+   fseek(file,44,SEEK_SET);
+   int num;
+   fread(&num,sizeof(int),1,file);
+   stereoChannel = malloc(num*sizeof(short));
+   fseek(file,4,SEEK_CUR);
+   fread(stereoChannel,sizeof(short),num,file);
+      printf("wavfile in stereo\n");
+      for (int inc=0;inc<header->Subchunk2Size/2;inc++){
+         if(choice[0]=='l'){
+          if(stereoChannel[inc*2]!=0){   
+            stereoChannel[inc*2]=0;
+            counter++;
+          }
+         }else if(choice[0]=='r'){
+          if(stereoChannel[(inc*2)+1]!=0){
+            stereoChannel[(inc*2)+1]=0;
+            counter++;
+          }
+         }
+      }
+
+      printf("<%d> samples on the <%s> channel were replaced with 0",counter,choice);
+  
+    
+}
 //3-ta tochka
 void part3(int sound_content_size){
    if(unit>='a'){
@@ -46,19 +87,15 @@ void part3(int sound_content_size){
    default:
       break;
    }
-   printf("the sound of content is %d %cB",sound_content_size,unit);
+   printf("the sound of content is %d %cB \n",sound_content_size,unit);
 
 }
 //2-ra tochak ot zadacahta
-void part2(struct wav_header*header){
- if(s_muted==0){   
-    printf("number of channels:%d\n",header->NumChannels);
-    }
-   
+void part2(struct wav_header*header){  
+    printf("number of channels:%d\n",header->NumChannels);     
 }
 //1-va tochka ot zadacahta
 void part_1(char **file,int argc, char *argv[]){
-   *file=argv[1];
    for(int arg=1;arg<argc;arg++){
    switch ((argv[arg])[0])
    {
@@ -66,105 +103,61 @@ void part_1(char **file,int argc, char *argv[]){
       s_muted=1;
       break;
    case 'f':
+       arg++;
+       *file=argv[arg];
        if(s_muted==0){ 
        printf("%s \n",*file);
        }
       break;
    case 'u':
       arg++;
-      unit=(argv[arg])[0];  
+      unit=(argv[arg])[0]; 
       break;
    } 
    }
 }
 
-//C:\\Users\\paco\\Desktop\\wav-channel-muter\\cheers.wav
 //C:\\Users\\paco\\Desktop\\wav-channel-muter\\test.wav
+//C:\\Users\\paco\\Desktop\\wav-channel-muter\\slay.wav
+//C:\\Users\\paco\\Desktop\\wav-channel-muter\\poe_whisper.wav
+
 int main(int argc, char *argv[])
 {
     //
-    char *file_name="C:\\Users\\paco\\Desktop\\wav-channel-muter\\test.wav";
+    char *file_name="poe_wispers.wav";
     FILE *file;
+    //
+    part_1(&file_name,argc,argv);
     file=fopen(file_name,"rb");
-   //
+    //
     fseek(file, 0L, SEEK_END);//otiva na kraya na faila
     int size = ftell(file); 
-   //
+    //
     char *data=malloc(size);
     fseek(file, 0L, SEEK_SET);//otiva v nachaloto na faila
     fread(data,1,size,file);//iska size za tova pochvame ot  purvo ot kraya i posle se vrushtame v nachaloto
 
     struct wav_header*header=(struct wav_header*)data;
     
-   //
-    part_1(&file_name,argc,argv);
+    //
+    if(s_muted==0){
     part2(header);
+
     int sound_content_size=header->Subchunk2Size;
     part3(sound_content_size);
-    //PlaySound(file_name,NULL,SND_SYNC|SND_FILENAME);
     
-  
-/*short * stereoChannel;
+    if(header->NumChannels==2){
+     printf("stereo \n");
+       
+       part4(file,header);
+    } 
+    }
+    PlaySound(file_name,NULL,SND_SYNC|SND_FILENAME);
+    //PlaySound(file_name,NULL,SND_FILENAME|SND_MEMORY);
 
-short * leftChannel;
-short * rightChannel;
-
-char    Subchunk2[4];
-int     move    =    0;
-short   channel =    1;
-int     val     =    50000;
-
-int main(){
-
-   FILE *fp = fopen("C:\\Users\\paco\\Desktop\\wav-chan\\test.wav","rb+");
-   if (!fp){
-      printf("Couldn't open file!\n");
-      return 0;
-   }
-   while (1){
-     fseek(fp,36+move,SEEK_SET);
-     fread(Subchunk2,4*sizeof(char),1,fp);
-     if (!strcmp(Subchunk2,"data")){
-        break;
-     }
-     move++;
-   }
-   fseek(fp,22,SEEK_SET);
-   fread(&channel,sizeof(short),1,fp);
-   fseek(fp,36+move+4,SEEK_SET);
-   int num;
-   fread(&num,sizeof(int),1,fp);
-   stereoChannel = malloc(num*sizeof(short));
-   fseek(fp,4,SEEK_CUR);
-   fread(stereoChannel,sizeof(short),num,fp);
-   if (channel == 2){
-      val=0;
-      printf("wavfile in stereo\n");
-      leftChannel = malloc(num*sizeof(short)/2);
-      rightChannel = malloc(num*sizeof(short)/2);
-      int channelpoint = 0;
-      for (int inc=0;inc<num/2;inc++){
-         leftChannel[channelpoint] = stereoChannel[inc*2];
-         rightChannel[channelpoint] = stereoChannel[(inc*2)+1];
-         channelpoint++;
-      }
-
-
-
-      for (int k=0; k<50000; k++){
-         printf("stereo sample %d L: %d R: %d \n",k,leftChannel[k],rightChannel[k]);
-      }
-
-      free(rightChannel);
-      free(leftChannel);
-   }
-   for (int k=0; k<val; k++){  // If this is stereo, it would print nothing because val was set to 0
-      printf("mono sample %d is %d\n", k, stereoChannel[k]);
 }
-   free(stereoChannel);
-   return 0;
-}
+ 
 
-// FILE *fp = fopen("C:\\Users\\paco\\Desktop\\wav-chan\\cheers.wav","rb+");
-*/
-}
+
+
+
