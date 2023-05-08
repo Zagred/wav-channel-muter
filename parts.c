ba5730b5d84a2547f3e0b44302b286299cebb620
paco <74721116+Zagred@users.noreply.github.com>
2023-05-09 00:12:34 +0300

    Add files via upload

--- parts.c
+++ parts.c
@@ -7,7 +7,7 @@
 #include <conio.h>
 #include <sys/stat.h>
 #pragma pack(push,1)
-struct wav_header{//struktura na celia header int-4 short-2
+struct wav_header{//header struct int-4 short-2
    int Chunk_ID;
    int Chunk_Size;
    int Format;
@@ -27,6 +27,16 @@ static int s_muted=0,s_unit=0,f_file=0;
 static char unit='B',choice[1000],wav_name[1000];
 
 //4-ta tochka
+//------------------------------------
+// DESCRIPTION:
+// converts the left or right sample on file into 0 and make it onlye 1 channel 
+// PARAMETERS:
+//struct wich contains header of the wav file
+// RETURN VALUE:
+//returns 2 things 
+//1:new file with muted one of the sides
+//2:how much sample are sets to 0(it means to be muted)
+//----
 void part4(struct wav_header*header){ 
   int choice_rl=0;
   while (1)
@@ -49,8 +59,8 @@ void part4(struct wav_header*header){
     printf("choice:%s\n",choice);
    }
     short *samples=(short*)((char*)header+44)+choice_rl;//
-    int counter=0;//koi sampali sa zameneni ot neshto kum 0
-      for (int inc=0;inc<header->Subchunk2Size/sizeof(short);inc+=2){//edin left/right sample e short(2 bite) i prezkachame s  2 za da hodi samo ot edinia vid
+    int counter=0;//counter for the samples wich we set to 0
+      for (int inc=0;inc<header->Subchunk2Size/sizeof(short);inc+=2){//one sample is 2 byte wich means to get left we will need to add 2 after every sample
          if(samples[inc]!=0){
           counter++;
          }
@@ -63,6 +73,14 @@ void part4(struct wav_header*header){
     
 }
 //3-ta tochka
+//------------------------------------
+// DESCRIPTION:
+//find sound content of wav file
+// PARAMETERS:
+// int value wich contains the number of sound content in b
+// RETURN VALUE:
+// sound content in size what we chose
+//----
 void part3(int sound_content_size){
   if(s_unit==1){ 
    if(unit>='a'){
@@ -101,12 +119,28 @@ void part3(int sound_content_size){
     
 }
 //2-ra tochak ot zadacahta
+//------------------------------------
+// DESCRIPTION:
+//finds in int format number of channels of the wav file
+// PARAMETERS:
+//struct wich contains header of the wav file
+// RETURN VALUE:
+// number of channels its only 1 or 2
+//----
 void part2(struct wav_header*header){  
    if(s_muted==0){  
       printf("number of channels:%d\n",header->NumChannels);
    }     
 }
 //1-va tochka ot zadacahta
+//------------------------------------
+// DESCRIPTION:
+//set the functions we  chose
+// PARAMETERS:
+//argc(int)  is number of position and argv(char) is the string on that position
+// RETURN VALUE:
+//2 values(for s and u function) if they are pressed and f function checks path for wav file
+//----
 void part_1(int argc, char *argv[]){
    for(int arg=1;arg<argc;arg++){
    switch ((argv[arg])[0])
